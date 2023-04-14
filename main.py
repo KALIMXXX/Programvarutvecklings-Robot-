2f8f92828dd7b8bd4bbd8c2f1ad82cdba2eab993
Omarhams <omar.hams@hotmail.com>
2023-04-14 13:59:54 +0200

    Djur b

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+import time
+
 
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
@@ -18,23 +20,49 @@ turning_motor = Motor(Port.C)
 arm_motor = Motor(Port.B)
 claw_motor = Motor(Port.A)
 color_sensor = ColorSensor(Port.S2)
+touch_sensor = TouchSensor(Port.S1)
 
-# Write your program here.
-ev3.speaker.beep()
-arm_motor.run_angle(100,-300, then=Stop.HOLD, wait=False)
+def pick_up_angle(given_angle):
 
-turning_motor.run_until_stalled(-100, then=Stop.HOLD, duty_limit=None)
+    # ev3.speaker.beep()
+    arm_motor.run_angle(300,-400, then=Stop.HOLD, wait=True)
 
-current_color = None
+    while touch_sensor.pressed() == False:
+        turning_motor.run_angle(300, 20, then=Stop.HOLD, wait=True)
 
-while (current_color == None and turning_motor.stalled() == False):
-    color = color_sensor.color()
-    turning_motor.run_angle(300,-10, then=Stop.HOLD, wait=True)
+    turning_motor.reset_angle(0)
 
-claw_motor.run_until_stalled(300, then=stop.HOLD, duty_limit=None)
+    desiered_angle = given_angle
 
-arm_motor.coast()
+    turning_motor.run_target(200,(desiered_angle*-3.5))
 
-claw_motor.run_until_stalled(-300, then=stop.HOLD, duty_limit=None)
+    """Turning motor to right angle"""
 
-arm_motor.run_until_stalled(300, then=stop.HOLD, duty_limit=None)
+    claw_motor.run_until_stalled(-50, then=Stop.COAST, duty_limit=50)
+    claw_motor.run_angle(50,70, then=Stop.HOLD, wait=True)
+
+
+    arm_motor.run_angle(500,100, then=Stop.COAST, wait = False)
+    time.sleep(4)
+
+    claw_motor.run_until_stalled(-50, then=Stop.HOLD, duty_limit=50)
+
+    arm_motor.run_angle(300,-400, then=Stop.HOLD, wait=True)
+
+    ooga=5
+
+    while ooga==5:
+        arm_motor.angle()
+    return
+
+def put_down():
+    arm_motor.run_angle(500,100, then=Stop.COAST, wait = False)
+    time.sleep(4)
+
+    claw_motor.run_angle(50, 70, then=Stop.HOLD, wait=True)
+    return
+
+
+
+
+
