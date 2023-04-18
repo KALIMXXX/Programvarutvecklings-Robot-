ffc6b0c0760c8eebcc83b7a7151e63b3135867dc
Omarhams <omar.hams@hotmail.com>
2023-04-18 05:09:54 +0200

    12

--- main.py
+++ main.py
@@ -22,6 +22,63 @@ claw_motor = Motor(Port.A)
 color_sensor = ColorSensor(Port.S2)
 touch_sensor = TouchSensor(Port.S1)
 
+def calibrate():
+
+        while touch_sensor.pressed() == False:
+        turning_motor.run_angle(300, 20, then=Stop.HOLD, wait=True)
+        turning_motor.reset_angle(0)
+
+    claw_motor.run_until_stalled(-50, then=Stop.HOLD, duty_limit=50)
+    claw_motor.reset_angle(0)
+    claw_motor.run_angle(50,70, then=Stop.HOLD, wait=True)
+
+    print(turning_motor.angle())
+    print(claw_motor.angle())
+
+    return
+
+def turn_angle(given_angle):
+
+    arm_motor.run_angle(300,-400, then=Stop.HOLD, wait=True)
+
+    while touch_sensor.pressed() == False:
+        turning_motor.run_angle(300, 20, then=Stop.HOLD, wait=True)
+
+    turning_motor.reset_angle(0)
+
+    desiered_angle = given_angle
+
+    turning_motor.run_target(200,(desiered_angle*-3.5))
+
+    """Turning motor to right angle"""
+    return
+
+def pick_up():
+    
+    claw_motor.run_until_stalled(-50, then=Stop.HOLD, duty_limit=50)
+    claw_motor.reset_angle(0)
+
+    claw_motor.run_angle(50,70, then=Stop.HOLD, wait=True)
+
+    arm_motor.run_angle(300,400, then=Stop.COAST, wait = True)
+    
+
+    claw_motor.run_until_stalled(-50, then=Stop.HOLD, duty_limit=50)
+
+    item_found = False
+
+    if claw_motor.angle() > 10:
+        item_found = True
+    else:
+        item_found = False
+    
+    arm_motor.run_angle(300,-400, then=Stop.HOLD, wait=True)
+    
+    return item_found
+
+
+
+
 def pick_up_angle(given_angle):
 
     # ev3.speaker.beep()
@@ -38,7 +95,9 @@ def pick_up_angle(given_angle):
 
     """Turning motor to right angle"""
 
-    claw_motor.run_until_stalled(-50, then=Stop.COAST, duty_limit=50)
+    claw_motor.run_until_stalled(-50, then=Stop.HOLD, duty_limit=50)
+    claw_motor.reset_angle()
+
     claw_motor.run_angle(50,70, then=Stop.HOLD, wait=True)
 
 
@@ -47,10 +106,17 @@ def pick_up_angle(given_angle):
 
     claw_motor.run_until_stalled(-50, then=Stop.HOLD, duty_limit=50)
 
+    item_found = False
+
+    if claw_motor.angle() > 10:
+        item_found = True
+    else:
+        item_found = False
+    
     arm_motor.run_angle(300,-400, then=Stop.HOLD, wait=True)
+    return item_found
 
-    ooga=5
-
+   
 
 def put_down():
     arm_motor.run_angle(300,400, then=Stop.COAST, wait = True)
@@ -67,7 +133,14 @@ def check_color():
     arm_motor.run_angle(300, -200, then=Stop.HOLD, wait = True)
     return
 
+def run_until_found(given_angle):
+    
+    item_found = False
+    while item_found == False:
+      turn_angle(given_angle):  
 
+    time.sleep(5)
+    return
 
 
 check_color()
